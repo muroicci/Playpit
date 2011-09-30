76a76b6215af952e0ff2a4af1f316521fe2d4e0d
Eiji Muroichi <muroicci@gmailc.om>
2011-09-30 14:17:03 -0700

    Change file/folder names and directory structure

---
+++ 4/main.js
@@ -0,0 +1,301 @@
+		
+		var container;
+		var camera;
+		var scene;
+		var renderer;
+		var geometry;
+		var group;
+
+		var particles = new Array();
+		var pPos = new Array();
+		var pVectors = new Array();
+		var vVectors = new Array();
+		var radius = new Array();
+		var particleNum = 200;
+		
+		var stageWidth = 500;
+		var stageHeight = 500;
+		var windowHalfX = stageWidth/2;
+		var windowHalfY = stageHeight/2;
+		var mx=0;
+		var my=0;
+		
+		var pi = Math.PI;
+		
+		var sphereR = 250;
+		var particleR = 15;
+
+		function init(){
+
+			if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+			
+			//container
+			container = document.createElement('div');
+			document.body.appendChild(container);
+			
+			//camera
+			camera = new THREE.Camera(75, window.innerWidth/window.innerHeight, 1, 10000);
+			camera.position.z = 400;
+			
+			//scene
+			scene = new THREE.Scene();
+			scene.fog = new THREE.Fog( 0x000000, 1, 900);
+			
+			group = new THREE.Object3D();
+			target = new THREE.Object3D();
+			
+			for( var i=0; i<particleNum; i++){
+				//create particles;
+				pPos[i] = new THREE.Vector3(0,0,0);
+				pVectors[i] = new THREE.Vector2(Math.random()*pi, Math.random()*pi);
+				vVectors[i] = new THREE.Vector2( 0, 0);
+				
+				var pr = particleR*Math.random()*2 + 1;
+				radius[i] = pr;
+				geometry = new THREE.CubeGeometry(pr, pr, 5);
+				//geometry = new THREE.CubeGeometry(particleR*2, particleR*2, particleR*2);
+				material = new THREE.MeshLambertMaterial({
+					color:0xcccccc,
+				});
+				
+				var mesh = new THREE.Mesh(geometry, material);
+				mesh.matrixAutoUpdate = false;
+				mesh.updateMatrix();
+				
+				particles[i] = mesh;
+				
+				group.addChild(mesh);
+			}
+			
+			//light
+			var light = new THREE.PointLight(0xffffff, 1);
+			light.position.x = -80;
+			light.position.y = 0;
+			light.position.z = 400;
+			
+			scene.addLight( light );
+			scene.addObject( group );
+			scene.addObject( target );
+			
+			//renderer
+			renderer = new THREE.WebGLRenderer({autoClear:false});
+			renderer.setSize( window.innerWidth, window.innerHeight);
+			renderer.sortObjects = false;
+			container.appendChild( renderer.domElement );
+			
+			
+			//event
+			document.addEventListener('mousemove', mouseMove);
+			window.addEventListener('resize', resize, false);
+			
+			//stats
+			stats = new Stats();
+			stats.domElement.style.position = 'absolute';
+			stats.domElement.style.top = '0px';
+			//container.appendChild( stats.domElement );
+			
+			animate();
+			
+		}
+		
+		function resize(){
+			stageWidth = window.innerWidth;
+			stageHeight = window.innerHeight;
+			windowHalfX = stageWidth/2;
+			windowHalfY = stageHeight/2;
+			camera.aspect = stageWidth/stageHeight;
+			renderer.setSize( stageWidth, stageHeight);
+			camera.updateProjectionMatrix();
+		}
+		
+		function mouseMove(ev){
+			omx = mx;
+			omy = my;
+			mx = ev.clientX - windowHalfX;
+			my = ev.clientY - windowHalfY;
+		}
+		
+		function animate(){
+			requestAnimationFrame(animate);
+			update();
+			render();
+//			stats.update();
+		}
+		
+		function render(){
+			
+//			camera.position.x += (mx - camera.position.x) * 0.05;
+			camera.position.y += (my/2+100 - camera.position.y) * 0.05;
+			group.rotation.y += (((mx-stageWidth*0.5)/10*pi/180  + 0 - group.rotation.y) * 0.05);
+			
+			renderer.render( scene, camera );
+		}
+		
+
+		var ofx = Math.random()*10;
+		var ofy = Math.random()*10;
+
+		function update(){
+			
+			ofx += 0.004;
+			ofy += 0.008;
+			
+			for(var i=0; i<particleNum; i++){
+				var mesh = particles[i];
+				var ps = pPos[i];
+				var p = pVectors[i];
+				var v = vVectors[i];
+				
+				//noise
+				var result = fBm2d( p.x/2 + ofx, p.y/2, 10);
+				v.x += (result)*0.000375;
+				v.y += (result)*0.000375;
+				
+				//set prop
+				
+				p.x += v.x;
+				p.y += v.y;
+
+				if(p.x>pi) {p.x =pi; v.x *=-1}
+				if(p.y>pi) {p.y =pi;v.y *=-1}
+				if(p.x<0) {p.x =0;v.x *=-1}
+				if(p.y<0) {p.y =0;v.y *=-1}
+
+			}
+			
+			
+			//colision
+			for(i=0;i<particleNum-1;i++){
+				
+				var p1 = pVectors[i];
+				var v1 = vVectors[i];
+				var r1 = radius[i];
+				
+				for( var j=i+1; j<particleNum; j++){
+					var p2 = pVectors[j];
+					var v2 = vVectors[j];
+					var r2 = radius[j];
+					
+					var ra = (r1*360/(2*pi*sphereR))　*　pi/180;
+					var rb = (r2*360/(2*pi*sphereR))　*　pi/180;
+					
+					var _a =     (v1.x * v1.x) - 2 * (v1.x * v2.x) +     (v2.x * v2.x) +     (v1.y * v1.y) - 2 * (v1.y * v2.y) +     (v2.y * v2.y);
+					var _b = 2 * (p1.x * v1.x) - 2 * (p1.x * v2.x) - 2 * (v1.x * p2.x) + 2 * (p2.x * v2.x) + 2 * (p1.y * v1.y) - 2 * (p1.y * v2.y) - 2 * (v1.y * p2.y) + 2 * (p2.y * v2.y);
+					var _c =     (p1.x * p1.x) - 2 * (p1.x * p2.x) +     (p2.x * p2.x) +     (p1.y * p1.y) - 2 * (p1.y * p2.y) +     (p2.y * p2.y) - (ra + rb) * (ra + rb);
+					var _d = _b * _b - 4 * _a * _c;
+					
+					if(_d<=0){
+						
+					}else{
+						
+						_d = Math.sqrt(_d);
+						var f0 = (- _b - _d) / (2 * _a);
+						var f1 = (- _b + _d) / (2 * _a);
+						var hit = false;
+						
+						if(f0>=0 && f0<=1){
+							
+							hit = true;
+							p1.x = p1.x + v1.x*f0;
+							p1.y = p1.y + v1.y*f0;
+							p2.x = p2.x + v2.x*f0;
+							p2.y = p2.y + v2.y*f0;
+							
+						}else if(f0*f1<0){
+							hit = true;
+							var vx = (p1.x - p2.x);
+							var vy = (p1.y - p2.y);
+							var len = Math.sqrt(vx*vx + vy*vy);
+							var distance = (ra+rb) - len;
+							
+							if(len>0) len = 1/len;
+							vx*=len;
+							vy*=len;
+							
+							distance /= 2.0;
+							p1.x += vx*distance;
+							p1.y += vy*distance;
+							p2.x -= vx*distance;
+							p2.y -= vy*distance;
+						}
+						
+						if(false && hit){
+						
+							var t;
+							var vx = (p2.x - p1.x);
+							var vy = (p2.y - p1.y);
+							
+							t = -(vx*v1.x + vy*v1.y) / (vx*vx+vy*vy);
+							var rx1 = v1.x + vx*t;
+							var ry1 = v1.y + vy*t;
+							
+							t = -(-vy*v1.x + vx*v1.y) / (vy*vy+vx*vy);
+							var mx1 = v1.x - vy*t;
+							var my1 = v1.y + vx*t;
+						
+							t = -(vx*v2.x + vy*v2.y) / (vx*vx + vy*vy);
+							var rx2 = v2.x + vx*t;
+							var ry2 = v2.y + vy*t;
+							
+							t = -(-vy*v2.x + vx*v2.y) / (vy*vy + vx*vy);
+							var mx2 = v2.x - vy*t;
+							var my2 = v2.y + vx*t;
+							
+							var e = 0.8;
+							var m = 10;
+							var dx1 = (m * mx1 + m * mx2 + mx2 * e * m - mx1 * e * m) / (m + m);
+							var dx2 = - e * (mx2 - mx1) + dx1;
+							var dy1 = (m * my1 + m * my2 + my2 * e * m - my1 * e * m) / (m + m);
+							var dy2 = - e * (my2 - my1) + dy1;
+							
+							v1.x = dx1+rx1;
+							v1.y = dy1+ry1;
+							v2.x = dx2+rx2;
+							v2.y = dy2+ry2;
+							
+						}
+						
+						if(p.x>pi) {p.x =pi; v.x *=-1}
+						if(p.y>pi) {p.y =pi;v.y *=-1}
+						if(p.x<0) {p.x =0;v.x *=-1}
+						if(p.y<0) {p.y =0;v.y *=-1}
+						
+						
+					}
+					
+				}
+				
+			}
+			
+			
+			for (i=0; i<particleNum; i++){
+				var ps = pPos[i];
+				var p = pVectors[i];
+				var mesh = particles[i];
+
+				var px = p.x;
+				var py = p.y;
+				
+				 ps.x = sphereR*Math.sin(py)*Math.cos(px);
+				 ps.y = sphereR*Math.sin(py)*Math.sin(px);
+				 ps.z = sphereR*Math.cos(py);
+				
+				mesh.position.x = ps.x;
+				mesh.position.y = ps.y;
+				mesh.position.z = ps.z;
+				mesh.lookAt(target.position);
+				mesh.updateMatrix();
+				
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
---
+++ 7/main.js
@@ -0,0 +1,311 @@
+		
+		var container;
+		var camera;
+		var scene;
+		var renderer;
+		
+		//for cube mapping
+		var cameraCube;
+		var sceneCube;
+		
+		var particleGeometry;
+		var particles;
+		var particleMaterial;
+
+		var lineGeometry;
+		var lineMaterial;
+		var line;
+		var lineColors = new Array();
+
+		var pPos = new Array();
+		var oPos = new Array();
+		var pVectors = new Array();
+		var vVectors = new Array();
+		var rpVectors = new Array();
+		var rvVectors = new Array();
+		
+		var rotationSpeed = 0;
+		
+		var stageWidth = 500;
+		var stageHeight = 500;
+		var mx=0;
+		var my=0;
+		
+		var pi = Math.PI;
+		
+		var sphereR = 100;
+		var particleR = 8;
+		var particleNum = 300;
+		var strokeNum = 50;
+
+		function init(){
+
+			if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+			
+			//container
+			container = document.createElement('div');
+			document.body.appendChild(container);
+			
+			//camera
+			camera = new THREE.Camera(75, window.innerWidth/window.innerHeight, 1, 10000);
+			camera.position.z = 400;
+			
+			//scene
+			scene = new THREE.Scene();
+			scene.fog = new THREE.Fog( 0x222222, 1, 800);
+			
+			target = new THREE.Object3D();
+			camera.target = target;
+			
+			//for cubic map
+			// cameraCube = new THREE.Camera(75, window.innerWidth/window.innerHeight, 1, 10000);
+			// camera.position.z = 400;
+			// sceneCube = new THREE.Scene();
+			// 
+			// var urls = [];
+			// var textureCube = THREE.ImageUtils.loadTextureCube( "textures/background.jpg", new THREE.CubeRefractionMapping() );
+			// var shader = THREE.ShaderUtils.lib["cube"];
+			// shader.uniforms["tCube"].texture = textureCube;
+			// var material = new THREE.MeshShaderMaterial({
+			// 	fragmentShader: shader.fragmentShader,
+			// 	vertexShader: shader.vertexShader,
+			// 	uniforms: shader.uniform
+			// });
+			// mesh = new THREE.Mesh( new THREE.CubeGeometry(1000, 1000, 1000, 1,1,1,null,true), material );
+			// sceneCube.addObject( mesh );			
+			
+			//Material
+			var texture = THREE.ImageUtils.loadTexture( "textures/100px_circle.png");
+			particleMaterial = new THREE.ParticleBasicMaterial({
+				color:0xffffff,
+				map:texture,
+				depthTest:false,
+				blending:THREE.AdditiveBlending,
+				size:particleR,
+				opacity:0.8,
+				transparent:true
+			});
+			
+			lineMaterial = new THREE.LineBasicMaterial({
+					vertexColors:true,
+					color:0xffffff,
+					blending:THREE.AdditiveBlending,
+					opacity: 1.0,//0.1,
+					lineWidth:1
+				});
+			
+			//create particles
+			particleGeometry = new THREE.Geometry();
+			lineGeometry = new THREE.Geometry();
+			
+			for( var i=0; i<particleNum; i++){
+				
+				//create vectors;
+				pVectors[i] = new THREE.Vector2(0.5*Math.random()*pi, 0.5*Math.random()*pi);
+				vVectors[i] = new THREE.Vector2( 0, 0);
+				pPos[i] = new THREE.Vector3(sphereR*Math.sin(pVectors[i].y)*Math.cos(pVectors[i].x), sphereR*Math.sin(pVectors[i].y)*Math.sin(pVectors[i].x), sphereR*Math.cos(pVectors[i].y));
+				particleGeometry.vertices.push( new THREE.Vertex(pPos[i]));
+				rpVectors[i] = sphereR;
+				rvVectors[i] = 0;
+				
+				for( var j=0; j<strokeNum; j++){
+					oPos.push(new Array());
+					oPos[i].push(pPos[i].clone());
+					lineGeometry.vertices.push( new THREE.Vertex(oPos[i][j]) );
+					
+					if(j>0){
+						//colors
+						var lc = new THREE.Color( 0xffffff );
+						lc.setHSV( 0.0, 0.0, (-0.47/strokeNum*j + 0.6)*1 );
+						lineColors.push( lc );
+						lineColors.push( lc );
+					}
+					
+				}
+				
+			}
+			
+			particles = new THREE.ParticleSystem( particleGeometry, particleMaterial );
+			particles.sortParticles = true;
+			scene.addObject( particles );
+			
+
+			line = new THREE.Line( lineGeometry, lineMaterial, THREE.LinePieces );
+			line.colors = lineColors;
+			scene.addObject( line );
+			
+			//renderer
+			renderer = new THREE.WebGLRenderer( { antialias:true, autoClear:false } );
+			renderer.setSize( window.innerWidth, window.innerHeight);
+			renderer.sortObjects = true;
+			container.appendChild( renderer.domElement );
+			
+			//Post proceccing
+			//var vignettEffect = new THREE.ShaderPass( THREE.ShaderExtras( "vignette" ) );
+			
+			//event
+			document.addEventListener('mousemove', mouseMove);
+			document.addEventListener('click', mouseClick);
+			window.addEventListener('resize', resize, false);
+			
+			//stats
+			stats = new Stats();
+			stats.domElement.style.position = 'absolute';
+			stats.domElement.style.top = '0px';
+			container.appendChild( stats.domElement );
+			
+			animate();
+			
+		}
+		
+		function mouseClick (e) {
+			for (var i = rvVectors.length - 1; i >= 0; i--){
+				rvVectors[i] += 5*Math.random()+5;
+				rvVectors[i] *= 1.2;
+				if(rvVectors[i]<0) rvVectors[i]*= -1;
+			};
+			
+			rotationSpeed += 0.05*(2*Math.random()-1);
+			
+		}
+		
+		function resize(){
+			stageWidth = window.innerWidth;
+			stageHeight = window.innerHeight;
+			camera.aspect =  stageWidth/stageHeight;
+			renderer.setSize(stageWidth, stageHeight)
+			camera.updateProjectionMatrix();
+		}
+		
+		function mouseMove(ev){
+			omx = mx;
+			omy = my;
+			mx = ev.clientX - window.innerWidth/2;
+			my = ev.clientY - window.innerWidth/2;
+		}
+		
+		function animate(){
+			requestAnimationFrame(animate);
+			
+			camera.position.x += (mx/2+100 - camera.position.x) * 0.05;
+			camera.position.y += (my/2+100 - camera.position.y) * 0.05;
+			
+			update();
+			render();
+			stats.update();
+		}
+		
+		function render(){
+			renderer.clear();
+		//	renderer.render( sceneCube, cameraCube );
+			renderer.render( scene, camera );
+		}
+		
+
+		var cnt = 0;
+
+		function update(){
+			
+			var perlin = new ImprovedNoise();
+			
+			var speed = 1.00;
+			
+			var vertices = [];
+			
+			var refreshLine = (cnt++%3==0);
+			
+			if(refreshLine){
+				scene.removeChild( line );
+				lineGeometry = new THREE.Geometry();
+			}
+			
+			for(var i=0; i<particleNum; i++){
+				
+				var ptcl = particleGeometry.vertices[i];
+				var ps = pPos[i];
+				var p = pVectors[i];
+				var v = vVectors[i];
+				var rp = rpVectors[i];
+				var rv = rvVectors[i];
+
+				var z = i*10;
+				
+				//noise
+				var resultX = perlin.noise( v.x*speed,	v.y*speed, z)*2-1;
+				var mrv = Math.abs(rv)/10;
+				v.x = (mrv+0.6)*(i/particleNum+1)*resultX*pi/180;
+				v.y = (mrv+0.3)*(i/particleNum+1)*resultX*pi/180;
+				speed*=1.0002;
+				p.addSelf(v);
+				
+				//pull each other
+				for( var j=0; j<particleNum; j++){
+					if(i!=j){
+						var q = pVectors[j];
+						if( p.distanceTo(q) < 10*pi/180){
+							var vv = new THREE.Vector3();
+							vv = vv.sub(q, p);
+							vv.multiplyScalar( 0.0060 );
+							p.addSelf(vv);
+						}
+					}
+				}
+				
+				//rVectors
+				var a = sphereR - rp;
+				a *= 0.010;
+				rv += a;
+				rv *= 0.93;
+				rp += rv;
+				var ps2 = ps.clone();
+				ps2.normalize();
+				ps2.multiplyScalar( rp );
+				rpVectors[i] = rp;
+				rvVectors[i] = rv;
+				
+				//matrix
+				//position
+				var mtx = new THREE.Matrix4();
+				mtx.setPosition( ps2 );
+				
+				//rotation
+				var mtr = new THREE.Matrix4();
+				mtr.setRotationX( p.x );
+				var mtrr = new THREE.Matrix4();
+				mtrr.setRotationY( p.y );
+				
+				var m = mtr.multiplySelf(mtx);
+				m = mtrr.multiplySelf( m );
+				
+				ptcl.position = m.getPosition();
+				ps = ptcl.position;
+				
+				if(refreshLine){
+					oPos[i].unshift(ps.clone());
+					oPos[i].pop();
+
+					for( var j=0; j<strokeNum-1; j++){
+						vertices.push( new THREE.Vertex( oPos[i][j] ) );
+						vertices.push( new THREE.Vertex( oPos[i][j+1] ) );
+					}
+				}				
+			}
+			
+			//draw lines
+			if(refreshLine){
+				lineGeometry.vertices = vertices;
+				lineGeometry.colors = lineColors;
+				line = new THREE.Line( lineGeometry, lineMaterial, THREE.LinePieces );
+				scene.addChild( line );
+			}
+			//rotate objects
+			// particles.rotation.y += rotationSpeed;
+			// particles.rotation.x += rotationSpeed;
+			// line.rotation = particles.rotation;
+			//rotationSpeed += ( 0 - rotationSpeed)*0.04;
+			cnt++;
+			
+		}
+		
+		///////////////////////////////////////////
+			
