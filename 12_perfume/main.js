b9216e5a8d091e3ece9a80dcc58d7d81e30472e4
Eiji Muroichi <muroicci@gmail.com>
2012-04-16 00:38:53 -0700

    playpit 12 almost done, metaballism

--- 12_perfume/main.js
+++ 12_perfume/main.js
@@ -1,25 +1,26 @@
 var container;
 var camera;
 var cameraTarget;
-var light;
+var mainLight, spotLight;
 var scene;
 var renderer;
-var group	;
+var group;
 var mousex = 0,
 	mousey = 0;
 var trackball;
 
-var world, bp;
-var balls = [];
+var material;
 
 var metaballs = [];
 var metaballController;
-var metaballFieldSize = 100;
+var metaballFieldSize = 120;
 
 var SHADOW_MAP_WIDTH = 2048;
 var SHADOW_MAP_HEIGHT = 2048;
 
 var motions = [];
+
+var audio;
 
 $(function() {
 
@@ -34,14 +35,14 @@ $(function() {
 
 	//scene
 	scene = new THREE.Scene();
-	//scene.fog = new THREE.Fog(0xeeeeee, 1, 1000);
+	scene.fog = new THREE.Fog(0x000000, 500, 1000);
 	//group
 	group = new THREE.Object3D();
 	scene.add(group);
 
 	//camera
 	camera = new THREE.PerspectiveCamera(40, width / height, 1, 1000);
-	camera.position.set(-0, 300, -300)
+	camera.position.set(0, 100, -400)
 	scene.add(camera);
 
 	cameraTarget = new THREE.Object3D();
@@ -49,13 +50,21 @@ $(function() {
 
 
 	//light
+	mainLight = new THREE.DirectionalLight(0xffffff, 0.2);
+	mainLight.castShadow = true;
+	mainLight.position.set(0, 600, 0);
+	scene.add(mainLight);
+
+	spotLight = new THREE.SpotLight(0x00fffff, 10, 400);
+	//spotLight.castShadow = true;
+	scene.add(spotLight);
+
 	var ambient = new THREE.AmbientLight(0x999999);
 	scene.add(ambient);
 
-	var light = new THREE.DirectionalLight(0xffffff, 1);
-	light.castShadow = true;
-	light.position.set(0, 600, 0);
-	scene.add(light);
+	sublight = new THREE.DirectionalLight(0xffffff, 0.5);
+	sublight.position.set(0, -10, 0)
+	scene.add(sublight);
 
 	//renderer
 	renderer = new THREE.WebGLRenderer();
@@ -63,7 +72,7 @@ $(function() {
 	renderer.setSize(width, height);
 	renderer.shadowMapEnabled = true;
 	renderer.shadowMapSoft = true;
-	renderer.shadowMapDarkness = 0.10;
+	renderer.shadowMapDarkness = 1.00;
 	renderer.shadowMapWidth = SHADOW_MAP_WIDTH;
 	renderer.shadowMapHeight = SHADOW_MAP_HEIGHT;
 	renderer.setClearColorHex(0x000000, 1.0);
@@ -72,15 +81,26 @@ $(function() {
 	//stats
 	stats = new Stats();
 	stats.domElement.style.position = 'absolute';
-	container.appendChild(stats.domElement);
+	//container.appendChild(stats.domElement);
 
 	//trackball
 	trackball = new THREE.TrackballControls(camera, renderer.domElement);
+	//trackball = new THREE.RollControls(camera, renderer.domElement);
+
+	//audio
+	// audio = new Audio();
+	// audio.src = "perfume.mp3";
+	// audio.play();
+	frameBufferSize = 1024;
+	bufferSize = frameBufferSize / 2;
+	signal = new Float32Array(bufferSize);
+	peak = new Float32Array(bufferSize);
+
+	fft = new FFT(bufferSize, 44100);
+
 
 	//load bvh datas
 	loadBVHData('bvhfiles/aachan.bvh');
-	// loadBVHData('bvhfiles/kashiyuka.bvh');
-	// loadBVHData('bvhfiles/aachan.bvh');
 
 	createScene();
 	animate();
@@ -92,6 +112,81 @@ $(function() {
 
 });
 
+var frameBufferSize, bufferSize, signal, peak;
+var fft;
+var context;
+var source;
+var audioProcessor;
+var currentvalue = [];
+var maxvalue = [];
+
+function loadAudio() {
+
+	context = new webkitAudioContext();
+	source = context.createBufferSource();
+	audioProcessor = context.createJavaScriptNode(2048);
+	audioProcessor.onaudioprocess = audioAvailable;
+	source.connect(audioProcessor);
+	audioProcessor.connect(context.destination);
+
+	//load
+	var request = new XMLHttpRequest();
+	request.open("GET", "perfume.mp3", true);
+	request.responseType = "arraybuffer";
+
+	request.onload = function() {
+		source.buffer = context.createBuffer(request.response, false);
+		source.noteOn(0);
+		startTimes[0] = Date.now();
+	}
+
+	request.send();
+
+}
+
+
+function audioAvailable(event) {
+
+	// Copy input arrays to output arrays to play sound
+	var inputArrayL = event.inputBuffer.getChannelData(0);
+	var inputArrayR = event.inputBuffer.getChannelData(1);
+	var outputArrayL = event.outputBuffer.getChannelData(0);
+	var outputArrayR = event.outputBuffer.getChannelData(1);
+
+	var n = inputArrayL.length;
+	for (var i = 0; i < n; ++i) {
+		outputArrayL[i] = inputArrayL[i];
+		outputArrayR[i] = inputArrayR[i];
+		signal[i] = (inputArrayL[i] + inputArrayR[i]) / 2; // create data frame for fft - deinterleave and mix down to mono
+	}
+
+	// perform forward transform
+	fft.forward(signal);
+
+	var mult = 0;
+	for (var i = 0; i < bufferSize / 8; i++) {
+		magnitude = fft.spectrum[i]; // multiply spectrum by a zoom value
+		currentvalue[i] = magnitude;
+
+		if (magnitude > maxvalue[i]) {
+			maxvalue[i] = magnitude;
+		} else {
+			if (maxvalue[i] > 10) {
+				maxvalue[i]--;
+			}
+		}
+		if(i<bufferSize/32) mult += magnitude;
+	}
+	if (mult>2) {
+		var col = spotLightColors[ Math.floor(Math.random()*spotLightColors.length-1)];
+		spotLight.color = sublight.color = new THREE.Color(col);
+		spotLight.intensity = mult*16;
+		
+	}else{
+		spotLight.intensity *= 0.4;
+	}
+
+}
 
 var channels = [];
 var roots = [];
@@ -100,6 +195,7 @@ var secsPerFrames = [];
 var startTimes = [];
 var currentFrames = [];
 var nodes = [];
+var spotLightColors = [0x00fffff, 0xff0000, 0x00ff00, 0x0000ff, 0xffff00, 0xff7800];
 
 //load and parsing data: modified from Saqoosha's code "http://saqoo.sh/a/labs/perfume/2/"
 
@@ -110,13 +206,13 @@ function loadBVHData(url) {
 		nodes.push([]);
 		motions.push(data.split(/\s+/g));
 		channels.push([]);
-		var mot = motions[motions.length-1];
+		var mot = motions[motions.length - 1];
 		done = false;
 		while (!done) {
 			switch (mot.shift()) {
 			case 'ROOT':
 				roots.push(parseNode(mot));
-				group.add(roots[roots.length-1]);
+				group.add(roots[roots.length - 1]);
 				break;
 			case 'MOTION':
 				mot.shift();
@@ -129,7 +225,8 @@ function loadBVHData(url) {
 		}
 		startTimes.push(Date.now());
 		currentFrames.push(0);
-		createBlob(roots[roots.length-1]);
+		createBlob(roots[roots.length - 1]);
+		loadAudio();
 	});
 
 }
@@ -187,46 +284,61 @@ function resize() {
 function createScene() {
 
 	//material
+	// environment map
+	var path = "/common/images/textures/cube/Bridge2/";
+	var format = '.jpg';
+	var urls = [
+	path + 'posx' + format, path + 'negx' + format, path + 'posy' + format, path + 'negy' + format, path + 'posz' + format, path + 'negz' + format];
+
+	var reflectionCube = THREE.ImageUtils.loadTextureCube(urls);
+	reflectionCube.format = THREE.RGBFormat;
+
+	var refractionCube = new THREE.Texture(reflectionCube.image, new THREE.CubeRefractionMapping());
+	refractionCube.format = THREE.RGBFormat;
+
 	material = new THREE.MeshPhongMaterial({
-		color: 0x222222,
+		color: 0xdd0000,
 		specular: 0xdddddd,
 		ambient: 0x222222,
-		shininess: 0,
-		perPixel: true
+		shininess: 8,
+		perPixel: true,
+		metal:true
 	});
 
-	var floorMat = new THREE.MeshNormalMaterial();
-
+	//material = new THREE.MeshLambertMaterial( { color: 0xffffff, envMap: reflectionCube, refractionRatio: 0.1 } );
+	//material = new THREE.MeshLambertMaterial( { color: 0xffffff, envMap: reflectionCube } )
+	//material = new THREE.MeshPhongMaterial( { color: 0x000000, specular: 0xffffff, envMap: reflectionCube, combine: THREE.MixOperation, reflectivity: 0.3, perPixel: true, metal: true } );
+	//material = new THREE.MeshPhongMaterial( { color: 0x550000, specular: 0x440000, envMap: reflectionCube, combine: THREE.MixOperation, reflectivity: 0.3, perPixel: true, metal: true } );
 	//ground
 	var geometry = new THREE.PlaneGeometry(10000, 10000);
 	var ground = new THREE.Mesh(geometry, material);
 	ground.castShadow = true;
 	ground.receiveShadow = true;
 	ground.rotation.x = -Math.PI / 2;
-	ground.position.y = -3;
+	ground.position.y = 0;
 	group.add(ground);
 
 
 	//Marching Cubes
 	metaballController = {
-		isolation: 1000,
-		resolution: 22,
-		subtract: 12,
-		strength: 1.5
+		isolation: 2300,
+		resolution: 26,
+		subtract: 30,
+		strength: 2.05
 	}
 
 	//gui
-	gui = new DAT.GUI();
-	var is = gui.add(metaballController, "isolation", 0, 24000, 1);
-	var rs = gui.add(metaballController, "resolution", 8, 128, 1);
-	var sb = gui.add(metaballController, "subtract", 1, 30, 1);
-	var st = gui.add(metaballController, "strength", 1, 5, 0.01);
+	// gui = new DAT.GUI();
+	// var is = gui.add(metaballController, "isolation", 0, 24000, 1);
+	// var rs = gui.add(metaballController, "resolution", 8, 128, 1);
+	// var sb = gui.add(metaballController, "subtract", 1, 30, 1);
+	// var st = gui.add(metaballController, "strength", 1, 5, 0.01);
 
 
 }
 
-function createBlob(root){
-	
+function createBlob(root) {
+
 	var mb = new THREE.MarchingCubes(metaballController.resolution, material)
 	metaballs.push(mb);
 	mb.castShadow = true;
@@ -235,34 +347,43 @@ function createBlob(root){
 	mb.position = root.position;
 	mb.scale.set(metaballFieldSize, metaballFieldSize, metaballFieldSize);
 	group.add(mb);
-	
-}
 
+}
 
 
 function animate() {
 	requestAnimationFrame(animate);
-	
-	//camera
-	trackball.update();
-	if(roots[0] != undefined ) camera.lookAt(roots[0].position)
 
-	for(var i=0; i<1; i++){
+	trackball.update();
+	if (roots[0] != undefined) {
+		//camera
+		camera.lookAt(roots[0].position)
+		//light
+		spotLight.position = roots[0].position.clone();
+		spotLight.position.y += 300;
+	}
+
+	for (var i = 0; i < 1; i++) {
 		updateBlobs(i);
 	}
+
 	render();
-	stats.update();
+	//stats.update();
 }
 
 function updateBlobs(i) {
 
 
-	if(channels[i]==undefined) return;
+	if (channels[i] == undefined) return;
 	//animation
 	var ch, frame, n, torad, _i, _len, _ref;
-
 	frame = ((Date.now() - startTimes[i]) / secsPerFrames[i] / 1000) | 0;
 	n = frame % numFrames[i] * channels[i].length;
+	if (frame >= numFrames[i]) {
+		startTimes[i] = Date.now();
+		loadAudio();
+	}
+
 	torad = Math.PI / 180;
 	_ref = channels[i];
 
@@ -335,7 +456,7 @@ function updateBlobs(i) {
 
 
 	}
-	metaballs[i].addPlaneY(5, 12);
+	metaballs[i].addPlaneY(50, 12);
 
 	metaballs[i].position = roots[i].position;
 
